a00cd7475023d09d401b6530558994e832fb707b
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-07 22:40:17 +0700

    add: setting db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,30 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { json } from 'body-parser';
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
+import mysql, { PoolOptions } from "mysql2"
 
 dotenv.config();
 
 const app: Application = express();
-app.use(json());
 const port = process.env.PORT || 3000;
+
+app.use(json());
+app.use(urlencoded({ extended: false }))
+
+const access: PoolOptions = {
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  database: process.env.DB_NAME || 'bank_db',
+}
+
+const conn = mysql.createPool(access)
+
+app.get('/attractions', (req: Request, res: Response) => {
+  conn.query('SELECT * FROM attractions = ?', req.params.id, (_err, rows) => {
+    res.send(rows)
+  })
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.status(201).send('Express + TypeScript Server');
