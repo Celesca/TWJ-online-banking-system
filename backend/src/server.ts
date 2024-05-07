8b308841fa3e6f8c36deb91752ee24fc699252b6
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-07 22:01:38 +0700

    setup: prettier format

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { json } from "body-parser";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(json());
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
-})
+  res.send("Express + TypeScript Server");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
