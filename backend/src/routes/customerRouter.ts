e2d094591638c0b376e0f8893001993c63f58907
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-12 22:24:09 +0700

    add: customerRouter

--- backend/src/routes/customerRouter.ts
+++ backend/src/routes/customerRouter.ts
@@ -5,11 +5,23 @@ import connection from 'db/dbconnection';
 export const customerRouter = Router();
 
 // customerRouter.post('/register', async (req: Request, res: Response) => {
-  
+
 //   })
 
 customerRouter.get('/', async (req: Request, res: Response) => {
   const sql_query = `SELECT * FROM customer`;
+
+  try {
+    const [rows] = await connection.query(sql_query);
+    return res.status(200).send(rows);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+customerRouter.get('/:username', async (req: Request, res: Response) => {
+  const username = req.params.username;
+  const sql_query = `SELECT * FROM customer WHERE username = ${username}`;
 
   try {
     const [rows] = await connection.query(sql_query);
--- backend/src/routes/personRouter.ts
+++ backend/src/routes/personRouter.ts
@@ -37,27 +37,36 @@ personRouter.post('/checkid', async (req: Request, res: Response) => {
 });
 
 personRouter.post('/register', async (req: Request, res: Response) => {
-  const { national_card_id, first_name, last_name, birth_date, phone_number, email, address }: CreatePersonDto = req.body;
-  
+  const { national_card_id, first_name, last_name, birth_date, phone_number, email, address }: CreatePersonDto =
+    req.body;
+
   const national_card_id_query = `SELECT * FROM person WHERE national_card_id = ?`;
 
   try {
-    const [rows] = await connection.query(national_card_id_query, [national_card_id])
+    const [rows] = await connection.query(national_card_id_query, [national_card_id]);
 
     if (Array.isArray(rows) && rows.length > 0) {
       return res.status(400).send({ message: 'Person with this national card id already exists' });
     }
   } catch (err) {
-      return res.status(500).send(err);
+    return res.status(500).send(err);
   }
 
-  if (!national_card_id || !first_name || !last_name || !birth_date || !phone_number || !email ||  !address) {
+  if (!national_card_id || !first_name || !last_name || !birth_date || !phone_number || !email || !address) {
     return res.status(400).json({ message: 'Please provide all the fields' });
   }
 
   const sql_query = `INSERT INTO person (national_card_id, first_name, last_name, birth_date, phone_number, email, address) VALUES (?, ?, ?, ?, ?, ?, ?)`;
   try {
-    await connection.query(sql_query, [national_card_id, first_name, last_name, birth_date, phone_number, email, address]);
+    await connection.query(sql_query, [
+      national_card_id,
+      first_name,
+      last_name,
+      birth_date,
+      phone_number,
+      email,
+      address,
+    ]);
     return res.status(200).send({ message: 'Person added successfully' });
   } catch (err) {
     return res.status(500).send(err);
@@ -78,14 +87,13 @@ personRouter.post('/remove', async (req: Request, res: Response) => {
   } catch (err) {
     return res.status(500).send(err);
   }
-
 });
 
-personRouter.get('/', async (req:Request, res:Response) => {
+personRouter.get('/', async (req: Request, res: Response) => {
   const sql_query = 'SELECT * FROM person';
   try {
     const [rows] = await connection.query(sql_query);
-    return res.status(200).send(rows)
+    return res.status(200).send(rows);
   } catch (err) {
     return res.status(500).send(err);
   }
