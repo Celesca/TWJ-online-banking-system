c67b8e204b80626f53bc4c9e41a4f91bb06a7130
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-14 16:41:51 +0700

    add: wallet System

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -5,9 +5,54 @@ interface Card {
   link: string;
 }
 
-import './HomePage.css';
+import { useEffect, useState } from "react";
+import "./HomePage.css";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const HomePage = () => {
+
+  const [balance, setBalance] = useState<number>(0)
+  let hasWallet = false;
+
+  // const loadAllWallet = async () => {
+  //   const response = await axios.get('http://localhost:3000/walletByCustomerId/1')
+  //   const data = response.data
+  //   console.log(data)
+  // }
+
+  const queryWallet = async (username: string) => {
+    const response = await axios.get('http://localhost:3000/wallet/' + username)
+    if (response.data.length > 0) {
+      setBalance(response.data[0].balance)
+      hasWallet = true
+    } else {
+      Swal.fire({
+        title: "No wallet found",
+        icon: "error",
+        timer: 1500,
+        showConfirmButton: false,
+      })
+    }
+  }
+
+  useEffect(() => {
+    document.title = "TWJ Online Banking - Home";
+    // Check the balance of the account
+    const username = localStorage.getItem("username");
+    if (username) {
+      queryWallet(username)
+    } else {
+      Swal.fire({
+        title: "Please login first",
+        icon: "error",
+        timer: 1500,
+        showConfirmButton: false,
+      }).then(() => window.location.href = "/login");
+    }
+
+  }, [])
+
   const cards: Card[] = [
     {
       title: "Deposit",
@@ -36,36 +81,61 @@ const HomePage = () => {
   ];
 
   return (
-    <div className="flex bg-gradient-to-r from-indigo-500 homepage_container p-16">
-    <div className="w-1/2 p-4">
-      <div className="bg-white rounded-lg shadow-lg p-6">
-        <h2 className="text-lg text-center font-semibold">Balance</h2>
-        <div className="flex justify-center">
-          <div className="text-4xl font-semibold text-center pt-4">฿ 1,000,000</div>
+    <div className="bg-gradient-to-r from-indigo-500 homepage_container p-16">
+       <header className="text-greetings text-4xl p-2">
+          What do you <span>want to do today?</span>
+        </header>
+      <div className="flex">
+        <div className="w-1/2 p-4 mt-4 balance-container">
+          <div className="bg-white rounded-lg shadow-lg p-12">
+            {hasWallet && (
+              <>
+                <h2 className="text-lg text-center font-semibold">Balance</h2>
+                <div className="flex flex-col justify-center">
+                  <div className="text-4xl font-semibold text-center pt-8">
+                    ฿ {balance.toFixed(2)}
+                  </div>
+                </div>
+              </>
+            )}
+            {!hasWallet && (
+              <div className="text-center">
+                
+                <h2 className="text-lg font-semibold">No wallet found</h2>
+                </div>
+              )}
+          </div>
+        </div>
+        <div className="w-1/2 p-4">
+          <div className="flex flex-wrap justify-center">
+            {cards.map((card, index) => (
+              <div
+                key={index}
+                className="w-1/2 min-w-0 sm:min-w-1/2 p-4 card-content"
+              >
+                <div className="bg-white rounded-lg shadow-lg pl-4 py-4 pr-0 flex">
+                  <div className="flex">
+                    <img
+                      src={card.image}
+                      alt={card.title}
+                      className="w-32 h-32"
+                    />
+                  </div>
+                  <div className="py-4">
+                    <h3 className="text-lg text-center font-semibold">
+                      {card.title}
+                    </h3>
+                    <p className="text-gray-500 text-center pt-2">
+                      {card.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-    <div className="w-1/2 p-4">
-      <header className="text-greetings text-4xl p-2 mb-8">
-        What do you <span>want to do today?</span>
-      </header>
-      <div className="flex flex-wrap justify-center">
-  {cards.map((card, index) => (
-    <div key={index} className="w-1/2 min-w-0 sm:min-w-1/2 p-4 card-content">
-      <div className="bg-white rounded-lg shadow-lg pl-4 py-4 pr-0 flex">
-        <div className="flex">
-          <img src={card.image} alt={card.title} className="w-32 h-32"/>
-        </div>
-        <div className="py-4">
-  <h3 className="text-lg text-center font-semibold">{card.title}</h3>
-  <p className="text-gray-500 text-center pt-2">{card.description}</p>
-</div>
-      </div>
-    </div>
-  ))}
-</div>
-    </div>
-  </div>
   );
 };
 
