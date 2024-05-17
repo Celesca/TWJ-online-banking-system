5cef401f90fab99b2b4074764853d0502bbcbfeb
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-17 12:46:09 +0700

    implement deposit amount

--- frontend/src/pages/DepositPage/DepositPage.tsx
+++ frontend/src/pages/DepositPage/DepositPage.tsx
@@ -1,10 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Swal, { SweetAlertIcon } from "sweetalert2";
 import React from "react";
 import "./DepositPage.css";
 import axios from "axios";
+import { WalletData } from "../../model/Wallet";
 
 const DepositPage = () => {
+
+  const [walletData, setWalletData] = useState<WalletData[]>([]);
+  const [selectedWallet, setSelectedWallet] = useState<number>(0);
+  const [amount, setAmount] = useState<number>(1);
 
   const responseSwal = (title: string, text: string, icon: SweetAlertIcon) => {
     return Swal.fire({
@@ -21,11 +26,15 @@ const DepositPage = () => {
       "http://localhost:3000/api/accounts/" + username
     );
     if (response.data.length > 0) {
-        console.log(response.data);
+        setWalletData(response.data);
     } else {
       responseSwal("No wallet found", "", "error");
     }
   };
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedWallet(parseInt(event.target.value));
+  }
 
   useEffect(() => {
     const role = localStorage.getItem("role");
@@ -37,8 +46,20 @@ const DepositPage = () => {
     queryWallet(localStorage.getItem("username") || "");
   }, []);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const response = await axios.post("http://localhost:3000/api/accounts/deposit", {
+      username: walletData[selectedWallet].customer_username,
+      amount: amount
+    });
+    if (response.status === 201) {
+      responseSwal("Deposit successfully", "", "success").then(() => {
+        window.location.href = "/";
+      });
+    } else {
+      responseSwal("Deposit failed", "", "error");
+    }
+
   };
 
   return (
@@ -54,44 +75,52 @@ const DepositPage = () => {
         <div className="mb-5">
           <form className="max-w-sm mx-auto">
             <label
-              htmlFor="countries"
+              htmlFor="wallets"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               เลือกบัญชีที่จะฝาก
             </label>
             <select
-              id="countries"
+              onChange={handleChange}
+              id="wallets"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option selected>Choose a country</option>
-              <option value="US">United States</option>
-              <option value="CA">Canada</option>
-              <option value="FR">France</option>
-              <option value="DE">Germany</option>
+              {walletData.map((wallet, index) => {
+                return (
+                  <option key={index} value={index}>
+                    {wallet.customer_username} - {wallet.account_type_name}
+                  </option>
+                );
+              })}
             </select>
           </form>
         </div>
         <div className="mb-5">
           <label
-            htmlFor="password"
+            htmlFor="amount"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Password
+            Amount
           </label>
           <input
-            name="password"
-            type="password"
-            id="password"
+            name="amount"
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(parseInt(e.target.value))}
+            min={1}
+            id="amount"
             className="outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
           />
         </div>
+        <div className="flex justify-center pt-4">
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
         </button>
+        </div>
       </form>
     </div>
   );
