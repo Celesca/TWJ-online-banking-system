4e139fa97d79a4f596aeae0a03387a171a546309
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-21 16:53:07 +0700

    add: wallpaper

--- frontend/src/pages/WelcomePage/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage/WelcomePage.tsx
@@ -3,21 +3,26 @@ import './WelcomePage.css'
 
 const WelcomePage = () => {
   return (
-    <div className="bg-gradient-to-r from-indigo-500 homepage_container">
-        <div className="pt-16 text-center">
+    <div className="homepage_container">
+      <div className="flex flex-1 justify-evenly">
+        <div className="mt-12 text-center sub_container rounded-lg p-12 h-100">
           <h1 className="head_text text-white">TWJ Online Banking</h1>
+          <p className="sub_text text-white">Welcome to the future of banking</p>
+          <div className="flex justify-center mt-8">
+          <img src="digital_banking.svg" width={450} ></img>
+          </div>
         </div>
         <div className="login-container pt-16">
-        <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 focus:delay-300 transition delay-100 duration-300 ease-in-out font-medium rounded-lg text-sm px-12 py-4 text-center me-2 mb-2">
-          <Link to="/login">Login</Link>
-        </button>
-        <Link to="/register">
-          <div className="text-white text-lg m-12">
-          don't have account? <span className="underline decoration-solid text-blue-900">Sign up</span>
-          </div>
-          </Link>
+          <button type="button" className="text-white font-medium rounded-lg text-sm px-12 py-4 text-center me-2 mb-2">
+            <Link to="/login">Login</Link>
+          </button>
+          <Link to="/register">
+            <div className="text-white text-lg m-12">
+            don't have account? <span className="underline decoration-solid text-blue-900">Sign up</span>
+            </div>
+            </Link>
         </div>
-        
+        </div>
     </div>
   )
 }
