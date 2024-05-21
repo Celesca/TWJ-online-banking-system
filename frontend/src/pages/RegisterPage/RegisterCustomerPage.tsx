60ee9206b77090b2628850d3d0575887fad6c352
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-21 23:15:22 +0700

    redesign: register page

--- frontend/src/pages/RegisterPage/RegisterCustomerPage.tsx
+++ frontend/src/pages/RegisterPage/RegisterCustomerPage.tsx
@@ -2,12 +2,12 @@ import axios from "axios";
 import React, { useState } from "react";
 import Swal, { SweetAlertIcon } from "sweetalert2";
 import './RegisterCustomerPage.css';
+import { Link } from "react-router-dom";
 
 const RegisterCustomerPage = () => {
-  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [repassword, setRepassword] = useState<string>("");
-  const [salary, setSalary] = useState<number>(0);
 
   const responseSwal = async (title: string, text: string, icon: SweetAlertIcon) => {
     Swal.fire({
@@ -44,7 +44,7 @@ const RegisterCustomerPage = () => {
       );
       if (response.status === 201) {
         responseSwal("Success", "Register successfully!", "success").then(() => {
-          setUsername("");
+          setEmail("");
           setPassword("");
           setRepassword("");
           localStorage.removeItem("national_card_id");
@@ -53,7 +53,7 @@ const RegisterCustomerPage = () => {
 
       } else {
         responseSwal("Oops...", "Username already exists!", "error").then(() => {
-          setUsername("");
+          setEmail("");
           setPassword("");
           setRepassword("");
         })
@@ -67,24 +67,34 @@ const RegisterCustomerPage = () => {
       <div className="flex flex-1 justify-evenly">
         <div className="w-1/2">
             <section className="">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div className="flex flex-col items-center justify-center px-6 py-16 mx-auto">
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 register-container">
                     <div className="p-4 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Create an account
                         </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6 register-form" action="#">
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
+                                <input type="email" 
+                                name="email" id="email" 
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                                <input type="password" name="password" 
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                             </div>
                             <div>
                                 <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                                <input type="password" 
+                                value={repassword}
+                                onChange={(e) => setRepassword(e.target.value)}
+                                name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                             </div>
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
@@ -95,8 +105,8 @@ const RegisterCustomerPage = () => {
                                 </div>
                             </div>
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                            <p className="text-sm font-light text-white dark:text-dark">
+                                Already have an account? <Link to="/login"><a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a></Link>
                             </p>
                         </form>
                     </div>
@@ -105,8 +115,15 @@ const RegisterCustomerPage = () => {
           </section>
           </div>
   
-      <div className="w-1/2 flex">
-        Testing
+      <div className="w-1/2 pt-12">
+        <div className="promo-container flex flex-col flex-1 w-1/2">
+          <h1 className="text-xl">The journey begins!</h1>
+          <div className="image-container">
+          <img src="https://cdn.pixabay.com/photo/2024/01/13/09/18/coins-8505363_1280.jpg" width={600} className="border-x-emerald-200"></img>
+          </div>
+          <h2 className="text-xl pt-12">In the money we trust.</h2>
+          <h2 className="text-xl pt-2 pb-4">Choose <span className="brand-text">TWJ Online Banking</span></h2>
+        </div>
         </div>
       </div>
      
