5175a13100e12f11fde6a6a67344a52df33fd64f
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-21 17:13:45 +0700

    styling the welcome page continue

--- frontend/src/pages/WelcomePage/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage/WelcomePage.tsx
@@ -5,15 +5,18 @@ const WelcomePage = () => {
   return (
     <div className="homepage_container">
       <div className="flex flex-1 justify-evenly">
-        <div className="mt-12 text-center sub_container rounded-lg p-12 h-100">
+        <div className="mt-12 ml-12 text-center sub_container rounded-lg p-12 h-100">
           <h1 className="head_text text-white">TWJ Online Banking</h1>
-          <p className="sub_text text-white">Welcome to the future of banking</p>
+          <p className="sub_text text-white">Thailand No.1 Future banking</p>
           <div className="flex justify-center mt-8">
           <img src="digital_banking.svg" width={450} ></img>
           </div>
         </div>
-        <div className="login-container pt-16">
-          <button type="button" className="text-white font-medium rounded-lg text-sm px-12 py-4 text-center me-2 mb-2">
+        <div className="login-container py-16 pl-0 mt-8">
+          <h1 className="login-text">Welcome to new Era of Banking</h1>
+          <p className="login-sub-text text-2xl pt-4">Experience seamless banking with our modern online platform.</p>
+
+          <button type="button" className="text-white font-medium login-button text-sm px-12 py-4 mt-8 text-center me-2 mb-2">
             <Link to="/login">Login</Link>
           </button>
           <Link to="/register">
