5dff81c6a3ebcefe0ae3bc3381305054b5e6359a
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-22 16:21:25 +0700

    fix: WalletData

--- backend/src/routes/transferRouter.ts
+++ backend/src/routes/transferRouter.ts
@@ -1,10 +1,16 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { Request, Response } from "express";
-import connection from "../db/dbconnection";
+import { Request, Response } from 'express';
+import connection from '../db/dbconnection';
 
 export const transferRouter = Router();
 
 // Get all transfers
-transferRouter.get("/", async (req: Request, res: Response) => {
-    
+transferRouter.get('/', async (req: Request, res: Response) => {
+  try {
+    const [rows] = await connection.query('SELECT * FROM transfer');
+    return res.json(rows);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
