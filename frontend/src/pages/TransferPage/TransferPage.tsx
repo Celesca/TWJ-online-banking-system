fe7f3f06e5639649c8c06de25675edbfb10654d3
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-22 17:47:09 +0700

    add: balance in transfer page

--- frontend/src/pages/TransferPage/TransferPage.tsx
+++ frontend/src/pages/TransferPage/TransferPage.tsx
@@ -186,14 +186,17 @@ const TransferPage = () => {
         <div className="flex justify-center pt-4">
           <button
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Submit
           </button>
         </div>
       </form>
 
-      <img src="pocket.svg" className="p-1 ml-36" width={300}></img>
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <h1 className="text-3xl">Your balance : ฿ {walletData[selectedWallet].balance}</h1>
+        <img src="pocket.svg" className="p-1" width={300}></img>
+      </div>
       </div>
     </div>
   );
