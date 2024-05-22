101fe0b40716402263864100c6f365eceafb67d6
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-23 02:33:13 +0700

    fix: POST /transfers

--- backend/src/routes/transferRouter.ts
+++ backend/src/routes/transferRouter.ts
@@ -10,9 +10,37 @@ transferRouter.get('/', async (req: Request, res: Response) => {
   res.status(200).json('Get all transfers');
 });
 
+const transferInBank = async (from_account_id: string, to_account_id: string, amount: number) => {
+  connection.beginTransaction;
+  try {
+    const query = 'UPDATE account SET balance = balance - ? WHERE account_id = ?';
+    await connection.execute(query, [amount, from_account_id]);
+  
+    const query2 = 'UPDATE account SET balance = balance + ? WHERE account_id = ?';
+    await connection.execute(query2, [amount, to_account_id]);
+    connection.commit; 
+  } catch (error) {
+    connection.rollback;
+    return error;
+  }
+  return 
+}
+
+const transferOutsideBank = async (from_account_id: string, to_account_id: string, amount: number) => {
+  connection.beginTransaction;
+  try {
+    const query = 'UPDATE account SET balance = balance - ? WHERE account_id = ?';
+    await connection.execute(query, [amount, from_account_id]);
+    connection.commit; 
+  } catch (error) {
+    connection.rollback;
+    return error;
+  }
+  return 
+}
+
 transferRouter.post('/', async (req: Request, res: Response) => {
   const { amount, from_account_id, to_account_id, transaction_type_id } = req.body;
-  console.log(req.body);
   if (!amount || !from_account_id || !to_account_id || !transaction_type_id) {
     return res.status(400).json({ message: 'Invalid request body' });
   }
@@ -20,6 +48,13 @@ transferRouter.post('/', async (req: Request, res: Response) => {
   const query =
     'INSERT INTO transaction_tb (amount, from_account_id, to_account_id, transaction_type_id) VALUES (?, ?, ?, ?)';
   try {
+    if (transaction_type_id === 3) {
+      await transferInBank(from_account_id, to_account_id, amount);
+    }
+    else if (transaction_type_id === 2) {
+      await transferOutsideBank(from_account_id, to_account_id, amount);
+    }
+    
     const [rows] = await connection.execute(query, [amount, from_account_id, to_account_id, transaction_type_id]);
     return res.status(201).json(rows);
   } catch (error) {
