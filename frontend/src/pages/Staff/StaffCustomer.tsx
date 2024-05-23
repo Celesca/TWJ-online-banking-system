e2587cf4c75be570cf21e9bd061379ae41da6ca6
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-23 22:04:04 +0700

    🎉 add: staff_customers

--- frontend/src/pages/Staff/StaffCustomer.tsx
+++ frontend/src/pages/Staff/StaffCustomer.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react"
+
 const StaffCustomer = () => {
+    const [customerData, setCustomerData] = useState<customerData[]>([]);
+
   return (
     <div className="homepage_container">
      <div className="flex w-100vw header-container">
-        <h1 className="text-white text-3xl py-6 px-16">Transfer</h1>
+        <h1 className="text-white text-3xl py-6 px-16">Customers List</h1>
       </div>
   </div>
   )
